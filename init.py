f180b5bfdf234d6e56e0277d0945193539f211a5
Pinak Dhabu <pinakdhabu2005@gmail.com>
2026-04-09 16:38:32 +0530

    Add selection sort function and print statements

--- init.py
+++ init.py
@@ -1,1 +1,15 @@
 print("I Love NMIET!")
+print("Cloud Computing is the best Topic Ever!")
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+if __name__ == "__main__":
+    data = [64, 25, 12, 22, 11]
+    print("Original:", data)
+    print("Sorted:", selection_sort(data.copy()))
